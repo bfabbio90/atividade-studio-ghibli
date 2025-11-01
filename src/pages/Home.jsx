6ce453b6a891487801545ec31c87b3a964bcdd5b
Bruno Fabbio <brunobiodev@gmail.com>
2025-11-01 19:23:49 -0300

    refactor: revisa a estrutura principal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import logo from "../assets/logo.png";
+import imagem from "../assets/images.png"
 import "./Home.css";
 
 export default function Home() {
   return (
     <div className="home">
-      <img src={logo} alt="Studio Ghibli Logo" className="home-logo" />
+      <img src={imagem} alt="Studio Ghibli Logo" className="home-logo" />
       <h1>Bem-vindo ao Mundo Ghibli 🌿</h1>
       <p>
         Explore as animações mágicas do Studio Ghibli e descubra detalhes sobre
---
+++ src/pages/Sobre.jsx
@@ -0,0 +1,20 @@
+import "./Sobre.css";
+
+export default function Sobre() {
+  return (
+    <div className="sobre">
+      <h1>Sobre o Projeto</h1>
+      <p>
+        Este projeto foi desenvolvido como exercício prático para explorar React,
+        rotas e consumo de APIs. Os dados são fornecidos pela API pública do Studio Ghibli.
+      </p>
+
+      <h2>Integrantes do Grupo</h2>
+      <ul>
+        <li>👩‍💻 Bruno</li>
+        <li>👨‍💻 Thobias</li>
+        <li>👩‍💻 André</li>
+      </ul>
+    </div>
+  );
+}
